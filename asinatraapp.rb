3643daad4338236ce306b2b2a992d074c9e942c1
Paul Wilson <paulanthonywilson@gmail.com>
2010-09-28 15:21:06 +0100

    more environment details

--- asinatraapp.rb
+++ asinatraapp.rb
@@ -2,5 +2,11 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "<html><title>Your IP</title><body>Hello.  Your IP is #{ @env['REMOTE_ADDR'] }.  Thank you, and good night."
+  result = "<html><title><p>Your IP</title><body>Hello.  Your IP is #{ @env['REMOTE_ADDR'] }.<p>"
+  result << "<p>Other things of interest might be:-</p><ul>"
+  @env.each do |k,v|
+    result << "<li>#{ERB::Util.html_escape(k)} = #{ERB::Util.html_escape(v)}"
+  end
+  result << "  Thank you, and good night.</body></html>"
+
 end
