80a6c07e41fc149fc2fd6dc7da661530126ee938
Paul Wilson <paulanthonywilson@gmail.com>
2010-09-28 16:21:27 +0100

    sort and close list

--- asinatraapp.rb
+++ asinatraapp.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 get '/' do
   result = "<html><title><p>Your IP</title><body>Hello.  Your IP is #{ @env['REMOTE_ADDR'] }.<p>"
   result << "<p>Other things of interest might be:-</p><ul>"
-  @env.each do |k,v|
-    result << "<li>#{ERB::Util.html_escape(k)} = #{ERB::Util.html_escape(v)}"
-  end
-  result << "  Thank you, and good night.</body></html>"
+  result << @env.map do |k,v|
+    "<li>#{ERB::Util.html_escape(k)} = #{ERB::Util.html_escape(v)}"
+  end.sort.join
+  result << "</ul><p>  Thank you, and good night.</p></body></html>"
 
 end
